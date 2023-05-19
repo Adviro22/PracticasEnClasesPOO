88b34a42d8416a47f3a95e1c6f538aa8b65430c0
unknown <ajosueramos@gmail.com>
2023-05-19 12:21:36 -0500

    Avances

--- clase (16-05-2023)/classRolPagos.py
+++ clase (16-05-2023)/classRolPagos.py
@@ -15,48 +15,50 @@ class Empleado:
     def __str__(self):
         return f"{self.id} - {self.nombre} - {self.sueldo} - {self.cargo} - {self.fec_ing}"
     
-class Oficina:
-    def __init__(self, medico):
-        self.medico = medico
+class Administrativo:
+    def __init__(self, seguro_medico):
+        self.seguro_medico = seguro_medico
     def __str__(self):
-        return f"{self.medico}"
+        return f"{self.seguro_medico}"
 
 class Obrero:
-    def __init__(self, familia):
-        self.familia = familia
+    def __init__(self, contrato_colectivo):
+        self.contrato_colectivo = contrato_colectivo
     def __str__(self):
-        return f"{self.familia}"
+        return f"{self.contrato_colectivo}"
 
 class Sobretiempo:
-    def __init__(self, empleado, fecha, h50, h100, valor):
+    def __init__(self, empleado, aaaamm, h50, h100, valor):
         self.empleado = empleado
-        self.fecha = fecha
+        self.aaaamm = aaaamm
         self.h50 = h50
         self.h100 = h100
-        self.valor = valor
     def __str__(self):
-        return f"{self.empleado} - {self.fecha} - {self.h50} - {self.h100} - {self.valor}"
+        return f"{self.empleado} - {self.aaaamm} - {self.h50} - {self.h100}"
 
 class Nomina:
-    def __init__(self, id, fecha, toting, totdesc, neto):
+    def __init__(self, id, aaaamm, toting, totdesc, neto):
         self.id = id
-        self.fecha = fecha
+        self.aaaamm = aaaamm
         self.toting = toting
         self.totdesc = totdesc
         self.neto = neto
     def __str__(self):
-        return f"{self.id} - {self.fecha} - {self.toting} - {self.totdesc} - {self.neto}"
+        return f"{self.id} - {self.aaaamm} - {self.toting} - {self.totdesc} - {self.neto}"
     
 class Detalle:
-    def __init__(self, id, empleado, cargo, sueldo, sobretiempo, iess):
+    def __init__(self, id, empleado, cargo, sueldo, sobretiempo, iess, toting, totdesc, neto):
         self.id = id
         self.empleado = empleado
         self.cargo = cargo
         self.sueldo = sueldo
         self.sobretiempo = sobretiempo
         self.iess = iess
+        self.toting = toting
+        self.totdesc = totdesc
+        self.neto = neto
     def __str__(self):
-        return f"{self.id} - {self.empleado} - {self.cargo} - {self.sueldo} - {self.sobretiempo} - {self.iess}"
+        return f"{self.id} - {self.empleado} - {self.cargo} - {self.sueldo} - {self.sobretiempo} - {self.iess} - {self.toting} - {self.totdesc} - {self.neto}"
 
 
 
